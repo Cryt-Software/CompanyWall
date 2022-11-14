ed93f3669abd8d8a15b0e8675fe85e59f4ee137f
Adam Sever <avacadoadam@gmail.com>
2022-11-14 04:35:53 +0000

    Didnt remove the </a>

--- src/routes.js
+++ src/routes.js
@@ -650,211 +650,211 @@ async function getFinancialData(page) {
 // Tries to scrap Telephone, email and web address if present by clicking on show button
 // Waiting a bit, waiting for xpath and then extracting that info
 // This takes far too long almost 30 seconds should be reduced
-async function getContactDetails(page) {
-    // Weird thing was you had to click on the I not the button strange anti-webscraping measure more then likely
+// async function getContactDetails(page) {
+//     // Weird thing was you had to click on the I not the button strange anti-webscraping measure more then likely
 
-    //Not working will need more waits + click in the same time, and get data in one go.
-    // Click on all elements retry if errors
-    // Check if button where clicked
-    // then use same loop as directors and business information
+//     //Not working will need more waits + click in the same time, and get data in one go.
+//     // Click on all elements retry if errors
+//     // Check if button where clicked
+//     // then use same loop as directors and business information
 
-    // Add in catch e -> with tracking
+//     // Add in catch e -> with tracking
 
-    // Need to have better wait function or wait until function
+//     // Need to have better wait function or wait until function
 
-    // also add error handling for when the tel/web is show but not picked up by script
+//     // also add error handling for when the tel/web is show but not picked up by script
 
-    const xpathForTelText =
-        "//section/header/div/h3[contains(text(),'Kontakti')]/parent::div/parent::header/parent::section//div[@class='row']/div[1]//dt[text() = 'tel']";
-    const xpathForTelDD =
-        "//section/header/div/h3[contains(text(),'Kontakti')]/parent::div/parent::header/parent::section//div[@class='row']/div[1]//dt[text() = 'tel']/following-sibling::dd[1]";
+//     const xpathForTelText =
+//         "//section/header/div/h3[contains(text(),'Kontakti')]/parent::div/parent::header/parent::section//div[@class='row']/div[1]//dt[text() = 'tel']";
+//     const xpathForTelDD =
+//         "//section/header/div/h3[contains(text(),'Kontakti')]/parent::div/parent::header/parent::section//div[@class='row']/div[1]//dt[text() = 'tel']/following-sibling::dd[1]";
 
-    const xpathForEmailText =
-        "//section/header/div/h3[contains(text(),'Kontakti')]/parent::div/parent::header/parent::section//div[@class='row']/div[1]//dt[text() = 'email']";
-    const xpathForEmailDD =
-        "//section/header/div/h3[contains(text(),'Kontakti')]/parent::div/parent::header/parent::section//div[@class='row']/div[1]//dt[text() = 'email']/following-sibling::dd[1]";
+//     const xpathForEmailText =
+//         "//section/header/div/h3[contains(text(),'Kontakti')]/parent::div/parent::header/parent::section//div[@class='row']/div[1]//dt[text() = 'email']";
+//     const xpathForEmailDD =
+//         "//section/header/div/h3[contains(text(),'Kontakti')]/parent::div/parent::header/parent::section//div[@class='row']/div[1]//dt[text() = 'email']/following-sibling::dd[1]";
 
-    const xpathForWebText =
-        "//section/header/div/h3[contains(text(),'Kontakti')]/parent::div/parent::header/parent::section//div[@class='row']/div[1]//dt[text() = 'web']";
-    const xpathForWebDD =
-        "//section/header/div/h3[contains(text(),'Kontakti')]/parent::div/parent::header/parent::section//div[@class='row']/div[1]//dt[text() = 'web']/following-sibling::dd[1]";
+//     const xpathForWebText =
+//         "//section/header/div/h3[contains(text(),'Kontakti')]/parent::div/parent::header/parent::section//div[@class='row']/div[1]//dt[text() = 'web']";
+//     const xpathForWebDD =
+//         "//section/header/div/h3[contains(text(),'Kontakti')]/parent::div/parent::header/parent::section//div[@class='row']/div[1]//dt[text() = 'web']/following-sibling::dd[1]";
 
-    let TelEle = await page.$x(xpathForTelText);
-    let emailEle = await page.$x(xpathForEmailText);
-    let webEle = await page.$x(xpathForWebText);
+//     let TelEle = await page.$x(xpathForTelText);
+//     let emailEle = await page.$x(xpathForEmailText);
+//     let webEle = await page.$x(xpathForWebText);
 
-    let isTel = !(TelEle.length == 0);
-    let isWeb = !(webEle.length == 0);
-    let isEmail = !(emailEle.length == 0);
+//     let isTel = !(TelEle.length == 0);
+//     let isWeb = !(webEle.length == 0);
+//     let isEmail = !(emailEle.length == 0);
 
-    const extractValueFromSpan = (type) =>
-        `//section/header/div/h3[contains(text(),'Kontakti')]/parent::div/parent::header/parent::section//div[@class='row']/div[1]//dt[text() = '${type}']/following-sibling::dd[1]/div/span`;
+//     const extractValueFromSpan = (type) =>
+//         `//section/header/div/h3[contains(text(),'Kontakti')]/parent::div/parent::header/parent::section//div[@class='row']/div[1]//dt[text() = '${type}']/following-sibling::dd[1]/div/span`;
 
-    const buttonXpath = (type) =>
-        `//section/header/div/h3[contains(text(),'Kontakti')]/parent::div/parent::header/parent::section//div[@class='row']/div[1]//dt[text() = '${type}']/following-sibling::dd[1]//button//i`;
+//     const buttonXpath = (type) =>
+//         `//section/header/div/h3[contains(text(),'Kontakti')]/parent::div/parent::header/parent::section//div[@class='row']/div[1]//dt[text() = '${type}']/following-sibling::dd[1]//button//i`;
 
-    let telephone = "";
-    try {
-        if (isTel) {
-            logInfo("Telephone is listed");
-            const elements = await page.$x(buttonXpath("tel"));
-            // await page.waitFor(400);
-            if (elements.length == 0) {
-                logInfo("There is no Tel button ERROR");
-            } else {
-                await elements[0].click();
-                await page.waitFor(300);
-                try {
-                    await page.waitFor(extractValueFromSpan("tel"), {
-                        timeout: 350,
-                    });
-                } catch (e) {
-                    console.error("===== No telephone found timed out -----");
-                    console.error(e);
-                    console.error("==========================================");
-                }
-                logInfo("xpath has shown");
+//     let telephone = "";
+//     try {
+//         if (isTel) {
+//             logInfo("Telephone is listed");
+//             const elements = await page.$x(buttonXpath("tel"));
+//             // await page.waitFor(400);
+//             if (elements.length == 0) {
+//                 logInfo("There is no Tel button ERROR");
+//             } else {
+//                 await elements[0].click();
+//                 await page.waitFor(300);
+//                 try {
+//                     await page.waitFor(extractValueFromSpan("tel"), {
+//                         timeout: 350,
+//                     });
+//                 } catch (e) {
+//                     console.error("===== No telephone found timed out -----");
+//                     console.error(e);
+//                     console.error("==========================================");
+//                 }
+//                 logInfo("xpath has shown");
 
-                telephone = await helperGetInnerText(
-                    page,
-                    extractValueFromSpan("tel")
-                ); //TESTING
-                logInfo(`The telephone is ${telephone}`);
-            }
-        } else {
-            logInfo("There is no telephone listed");
-        }
-    } catch (e) {
-        console.error(e);
-        logInfo(
-            `ERROR PROCESSING telephone in contact scraper on URL ${page.url()}`
-        );
-    }
+//                 telephone = await helperGetInnerText(
+//                     page,
+//                     extractValueFromSpan("tel")
+//                 ); //TESTING
+//                 logInfo(`The telephone is ${telephone}`);
+//             }
+//         } else {
+//             logInfo("There is no telephone listed");
+//         }
+//     } catch (e) {
+//         console.error(e);
+//         logInfo(
+//             `ERROR PROCESSING telephone in contact scraper on URL ${page.url()}`
+//         );
+//     }
 
-    let email = "";
-    try {
-        if (isEmail) {
-            logInfo("Email is listed");
-            const elements = await page.$x(buttonXpath("email"));
-            // await page.waitFor(400);
-            if (elements.length == 0) {
-                logInfo("There is no email revail button ERROR");
-            } else {
-                await elements[0].click();
-                await page.waitFor(300);
-                try {
-                    await page.waitFor(extractValueFromSpan("email"), {
-                        timeout: 600,
-                    });
-                } catch (e) {
-                    console.error("===== No email found timed out -----");
-                    console.error(e);
-                    console.error("==========================================");
-                }
-                email = await helperGetInnerText(
-                    page,
-                    extractValueFromSpan("email")
-                ); //TESTING
-                logInfo(`The email is ${email}`);
-            }
-        } else {
-            logInfo("There is no email listed");
-        }
-    } catch (e) {
-        console.error(e);
-        logInfo(
-            `ERROR PROCESSING email in contact scraper on URL ${page.url()}`
-        );
-    }
+//     let email = "";
+//     try {
+//         if (isEmail) {
+//             logInfo("Email is listed");
+//             const elements = await page.$x(buttonXpath("email"));
+//             // await page.waitFor(400);
+//             if (elements.length == 0) {
+//                 logInfo("There is no email revail button ERROR");
+//             } else {
+//                 await elements[0].click();
+//                 await page.waitFor(300);
+//                 try {
+//                     await page.waitFor(extractValueFromSpan("email"), {
+//                         timeout: 600,
+//                     });
+//                 } catch (e) {
+//                     console.error("===== No email found timed out -----");
+//                     console.error(e);
+//                     console.error("==========================================");
+//                 }
+//                 email = await helperGetInnerText(
+//                     page,
+//                     extractValueFromSpan("email")
+//                 ); //TESTING
+//                 logInfo(`The email is ${email}`);
+//             }
+//         } else {
+//             logInfo("There is no email listed");
+//         }
+//     } catch (e) {
+//         console.error(e);
+//         logInfo(
+//             `ERROR PROCESSING email in contact scraper on URL ${page.url()}`
+//         );
+//     }
 
-    let webAddress = "";
-    try {
-        if (isWeb) {
-            logInfo("There is web listed");
+//     let webAddress = "";
+//     try {
+//         if (isWeb) {
+//             logInfo("There is web listed");
 
-            const elements = await page.$x(buttonXpath("web"));
-            await page.waitFor(400);
-            if (elements.length == 0) {
-                logInfo("There is no web button ERROR");
-            } else {
-                await elements[0].click();
-                await page.waitFor(300);
-                try {
-                    await page.waitFor(extractValueFromSpan("web"), {
-                        timeout: 750,
-                    });
-                } catch (e) {
-                    console.error("===== No website found timed out -----");
-                    console.error(e);
-                    console.error("==========================================");
-                }
-                webAddress = await helperGetInnerText(
-                    page,
-                    extractValueFromSpan("web")
-                ); //TESTING
+//             const elements = await page.$x(buttonXpath("web"));
+//             await page.waitFor(400);
+//             if (elements.length == 0) {
+//                 logInfo("There is no web button ERROR");
+//             } else {
+//                 await elements[0].click();
+//                 await page.waitFor(300);
+//                 try {
+//                     await page.waitFor(extractValueFromSpan("web"), {
+//                         timeout: 750,
+//                     });
+//                 } catch (e) {
+//                     console.error("===== No website found timed out -----");
+//                     console.error(e);
+//                     console.error("==========================================");
+//                 }
+//                 webAddress = await helperGetInnerText(
+//                     page,
+//                     extractValueFromSpan("web")
+//                 ); //TESTING
 
-                //TESTING XPATH ON WEB WITH XXX TO SEE IF BUTTON WAS CLICKED OR NOT
-                let testText = await helperGetInnerText(
-                    page,
-                    '//*[@id="main-content"]/div[3]/div[1]/section/div/div/div[1]/div/dl/dd[3]/div/span'
-                );
-                logInfo(`the test text is ${testText} `);
+//                 //TESTING XPATH ON WEB WITH XXX TO SEE IF BUTTON WAS CLICKED OR NOT
+//                 let testText = await helperGetInnerText(
+//                     page,
+//                     '//*[@id="main-content"]/div[3]/div[1]/section/div/div/div[1]/div/dl/dd[3]/div/span'
+//                 );
+//                 logInfo(`the test text is ${testText} `);
 
-                logInfo(`The website is ${webAddress}`);
-            }
-        } else {
-            logInfo("There is no Web");
-        }
-    } catch (e) {
-        console.error(e);
-        logInfo(
-            `ERROR PROCESSING Web address in contact scraper on URL ${page.url()}`
-        );
-    }
+//                 logInfo(`The website is ${webAddress}`);
+//             }
+//         } else {
+//             logInfo("There is no Web");
+//         }
+//     } catch (e) {
+//         console.error(e);
+//         logInfo(
+//             `ERROR PROCESSING Web address in contact scraper on URL ${page.url()}`
+//         );
+//     }
 
-    // test for active button but variable is empty
-    // test for active button but variable is of wrong format
+//     // test for active button but variable is empty
+//     // test for active button but variable is of wrong format
 
-    // ERROR CODE 1099
-    if (isEmail && email == "") {
-        console.error(
-            "ERROR CODE 1099 Email button is active but text is empty"
-        );
-        console.error(`Error 1099 at ${page.url()}`);
-    }
+//     // ERROR CODE 1099
+//     if (isEmail && email == "") {
+//         console.error(
+//             "ERROR CODE 1099 Email button is active but text is empty"
+//         );
+//         console.error(`Error 1099 at ${page.url()}`);
+//     }
 
-    if (isTel && telephone == "") {
-        console.error("ERROR CODE 1098 tel button is active but text is empty");
-        console.error(`Error 1097 at ${page.url()}`);
-    }
-    //ERROR CODE 1097
-    if (isWeb && webAddress == "") {
-        console.error("ERROR CODE 1097 web button is active but text is empty");
-        console.error(`Error 1097 at ${page.url()}`);
-    }
+//     if (isTel && telephone == "") {
+//         console.error("ERROR CODE 1098 tel button is active but text is empty");
+//         console.error(`Error 1097 at ${page.url()}`);
+//     }
+//     //ERROR CODE 1097
+//     if (isWeb && webAddress == "") {
+//         console.error("ERROR CODE 1097 web button is active but text is empty");
+//         console.error(`Error 1097 at ${page.url()}`);
+//     }
 
-    const validateEmail = (email) => {
-        return String(email)
-            .toLowerCase()
-            .match(
-                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-            );
-    };
-    // ERROR CODE 1096
-    if (validateEmail(email)) {
-        console.error(
-            `ERROR CODE 1096 Not valid email ${email}, This is at ${page.url()}`
-        );
-    }
+//     const validateEmail = (email) => {
+//         return String(email)
+//             .toLowerCase()
+//             .match(
+//                 /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+//             );
+//     };
+//     // ERROR CODE 1096
+//     if (validateEmail(email)) {
+//         console.error(
+//             `ERROR CODE 1096 Not valid email ${email}, This is at ${page.url()}`
+//         );
+//     }
 
-    let result = {
-        email: email,
-        telephone: telephone,
-        web: webAddress,
-    };
-    logInfo(result);
-    return result;
-}
+//     let result = {
+//         email: email,
+//         telephone: telephone,
+//         web: webAddress,
+//     };
+//     logInfo(result);
+//     return result;
+// }
 
 async function handleContactDetailsWithRequest(page) {
     // First get the IDC variable on all of the buttons.
@@ -922,7 +922,7 @@ async function handleContactDetailsWithRequest(page) {
         );
     },idc);
     try{
-        email = email.replace("</p>", "");
+        email = email.replace("</a>", "");
         email = email.replace(/<.*itemprop='email'>/i, "");
     }catch(e){
         console.error('error parsing the email contact value')
